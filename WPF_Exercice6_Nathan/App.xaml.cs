c2bf2f91cbe888b59ea6097a71a40c9aa571ad7b
WikiNat <nathan.marcq@icloud.com>
2025-09-21 23:27:26 +0200

    Fini le devoir

---
+++ WPF_Exercice6_Nathan/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_Exercice6_Nathan
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
